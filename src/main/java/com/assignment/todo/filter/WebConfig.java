df4dc18a12b1f1c6e84a6760eeb9ac6c873ab6ad
suho <sirius0991@gmail.com>
2024-12-24 10:24:34 +0900

    불필요한 코드 coutomFilter제거

--- src/main/java/com/assignment/todo/filter/WebConfig.java
+++ src/main/java/com/assignment/todo/filter/WebConfig.java
@@ -18,24 +18,25 @@ public class WebConfig {
      *
      * @return FilterRegistrationBean<CustomFilter> CustomFilter 필터 등록 객체
      */
-    @Bean
-    public FilterRegistrationBean customFilter() {
-        // CustomFilter 필터를 등록하고 필터 순서를 1로 설정 (순서는 숫자가 낮을수록 먼저 실행)
-        FilterRegistrationBean<CustomFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new CustomFilter()); // 필터 객체 설정
-        filterRegistrationBean.setOrder(1); // 필터의 실행 순서 설정
-        filterRegistrationBean.addUrlPatterns("/*"); // 모든 URL에 대해 필터를 적용
+//    @Bean
+//    public FilterRegistrationBean customFilter() {
+//        // CustomFilter 필터를 등록하고 필터 순서를 1로 설정 (순서는 숫자가 낮을수록 먼저 실행)
+//        FilterRegistrationBean<CustomFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+//        filterRegistrationBean.setFilter(new CustomFilter()); // 필터 객체 설정
+//        filterRegistrationBean.setOrder(1); // 필터의 실행 순서 설정
+//        filterRegistrationBean.addUrlPatterns("/*"); // 모든 URL에 대해 필터를 적용
+//
+//        // 필터 등록 정보를 반환
+//        return filterRegistrationBean;
+//    }
 
-        // 필터 등록 정보를 반환
-        return filterRegistrationBean;
-    }
-
+    //실질적으로 적용
     @Bean
     public FilterRegistrationBean loginFilter() {
         // LoginFilter 필터를 등록하고 필터 순서를 2로 설정 (1번 필터 후 실행)
         FilterRegistrationBean<LoginFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginFilter());
-        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
 
         return filterRegistrationBean;
